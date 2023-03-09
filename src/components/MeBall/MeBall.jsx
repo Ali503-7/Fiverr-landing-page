eb783c0865f41ef99f2fdf5bc43abb5d690666d0
Ali503-7 <alimoali354@gmail.com>
2023-03-09 19:06:36 +0200

    keep working on me-ball

--- src/components/MeBall/MeBall.jsx
+++ src/components/MeBall/MeBall.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './MeBall.scss'
+import React, {useState} from "react";
+import "./MeBall.scss";
 
 function MeBall() {
+  const [over, setOver] = useState(false);
+
+  function toggleOver() {
+    setOver((pre) => !pre);
+  }
   return (
-    <div className="me-ball">
+    <div className="me-ball" onMouseOver={() => toggleOver()}>
       <img src="imgs/MeBall/Yow.png" alt="" />
+      <div className="cont">
+        
+      <div className={over ? "papper active animation" : "papper"}>
+        <p>
+          Coded by{" "}
+          <a href="google.com" target="_blank">
+            Ali Mohamed
+          </a>
+        </p>
+        <p>
+          Challenge by{" "}
+          <a href="https://www.fiverr.com/?source=top_nav" target="_blank">
+            Fiverr.com
+          </a>
+        </p>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MeBall
+export default MeBall;
