ea5abd0725b8eae803d1e12e4a3ad3b5311d256e
Ali503-7 <alimoali354@gmail.com>
2023-03-11 15:29:54 +0200

    finsh every thing about the me-ball

--- src/components/MeBall/MeBall.jsx
+++ src/components/MeBall/MeBall.jsx
@@ -1,31 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./MeBall.scss";
 
 function MeBall() {
   const [over, setOver] = useState(false);
+  const [papper, setPapper] = useState(false);
 
-  function toggleOver() {
-    setOver((pre) => !pre);
+  function toggleOver(pram) {
+    setOver(pram);
   }
+
+  function papperAction(time, Boolean) {
+    setTimeout(() => {
+      setPapper(Boolean);
+    }, time);
+  }
+
+  function open() {
+    toggleOver(true);
+    papperAction(1000, true);
+  }
+
+  function close() {
+    papperAction(100, false);
+    setTimeout(() => {
+      toggleOver(false);
+    }, 1000);
+  }
+
+  const styleO = {
+    width: "130px",
+    marginLeft: "210px",
+    padding: "5px 10px",
+  };
+
+
   return (
-    <div className="me-ball" onMouseOver={() => toggleOver()}>
+    <div
+      className="me-ball"
+      onMouseEnter={() => open()}
+      onMouseLeave={() => close()}
+    >
       <img src="imgs/MeBall/Yow.png" alt="" />
       <div className="cont">
-        
-      <div className={over ? "papper active animation" : "papper"}>
-        <p>
-          Coded by{" "}
-          <a href="google.com" target="_blank">
-            Ali Mohamed
-          </a>
-        </p>
-        <p>
-          Challenge by{" "}
-          <a href="https://www.fiverr.com/?source=top_nav" target="_blank">
-            Fiverr.com
-          </a>
-        </p>
-      </div>
+        <div
+          className={over ? "papper push" : "papper"}
+          style={papper ? styleO : null}
+        >
+          <div className="textcon">
+            <p>
+              Coded by{" "}
+              <a href="google.com" target="_blank">
+                Ali Mohamed
+              </a>
+            </p>
+            <p>
+              Challenge by{" "}
+              <a href="https://www.fiverr.com/?source=top_nav" target="_blank">
+                Fiverr.com
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
